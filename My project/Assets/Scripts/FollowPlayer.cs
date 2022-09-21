b4798fc37d4912bdd05009a55f9c31cbf0349e37
smasher2o07 <asherbowdon@gmail.com>
2022-09-21 08:58:40 -0700

    i added code to more stuff

--- My project/Assets/Scripts/FollowPlayer.cs
+++ My project/Assets/Scripts/FollowPlayer.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 5.0f;
-
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +14,6 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // we will move the car forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position;
     }
 }
