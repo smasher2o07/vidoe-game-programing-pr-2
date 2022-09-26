0b18752bb47a6ccf30df569219198d53df701ada
smasher2o07 <asherbowdon@gmail.com>
2022-09-26 08:46:58 -0700

    code
    added more coder

--- My project/Assets/Scripts/PlayerControler.cs
+++ My project/Assets/Scripts/PlayerControler.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -14,8 +17,13 @@ public class PlayerControler : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // we will move the car forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
