cbb481f27f53f7c0796945f28e2c9b510edf39bf
smasher2o07 <asherbowdon@gmail.com>
2022-12-01 08:31:28 -0800

    added many componets like move left back round moves
    added many new scripts

---
+++ Prototype3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 15;
+    private PlayerController playerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("PLAYER").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+    }
+}
